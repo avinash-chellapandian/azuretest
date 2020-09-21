81e367dff98f451367dc72283a89b7eb0f9757ae
mohammedtabish <tabish.mohammed@accenture.com>
2020-09-21 13:41:47 +0530

    added controller

---
+++ src/main/java/com/helloworld/helloazure/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.helloworld.helloazure;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String test(){
+        return "Hello world from azure";
+    }
+}
